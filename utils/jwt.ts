ac4b482d04f3033699c06eafd137d51163f28d92
Jimmy Reyes <jrfullstack@gmail.com>
2023-04-27 04:35:53 +0200

    utilitis

---
+++ utils/jwt.ts
@@ -0,0 +1,43 @@
+import { rejects } from "assert";
+import jwt from "jsonwebtoken";
+import { resolve } from "path";
+
+export const signToken = (_id: string, email: string) => {
+
+    if (!process.env.JWT_SECRET_SEED){
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+    }
+
+    return jwt.sign(
+        // payload
+        {_id, email},
+
+        // seed
+        process.env.JWT_SECRET_SEED,
+
+        // opciones
+        {expiresIn: '30d'}
+    )
+}
+
+export const isValidToken = ( token: string ):Promise<string> => {
+    if (!process.env.JWT_SECRET_SEED) {
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+    }
+
+    if (token.length <= 10){
+        return Promise.reject('JWT no es válido');
+    }
+
+    return new Promise ( (resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) =>  {
+                if(err) return reject('JWT no es valido');
+                const {_id} = payload as {_id: string};
+                resolve(_id);
+            })
+        } catch (error) {
+            reject('JWT no es valido');
+        }
+    })
+}
