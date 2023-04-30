774cce8e397010ca080cb2eb7f90d7f58e876b30
Jimmy Reyes <jrfullstack@gmail.com>
2023-05-01 00:53:13 +0200

    cascaron audio

--- components/artistas/ArtistProfile.tsx
+++ components/artistas/ArtistProfile.tsx
@@ -15,65 +15,68 @@ export const ArtistProfile: FC<Props> = ({artista}) => {
     ));
 
 	return (
-		<Paper
-			elevation={4}
-			sx={{
-				paddingY: 3,
-				paddingX: 4,
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "space-between",
-			}}>
-			<Box>
-				<Avatar
-					alt={artista.interprete}
-					src={`/img/artistas/${artista.imgPortada}`}
-					sx={{
-						width: 150,
-						height: 150,
-						border: "2px solid #DE260E",
-					}}
-				/>
-			</Box>
-			<Box>
-				<Typography variant="h1" component="h1" color="initial">
-					{artista.interprete}
-				</Typography>
-				<List sx={{ textAlign: "center" }}>
-					{/* <ListItem> */}
-					{tipos}
-					{/* </ListItem> */}
-				</List>
-				<Box
-					sx={{
-						mt: 1,
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center",
-						color: "GrayText",
-					}}>
-					<FavoriteBorderOutlined fontSize="small" sx={{ pr: "3px" }} />
-					{artista.seguidores} Seguidores
-				</Box>
-			</Box>
-			<Box sx={{ display: "flex", flexDirection: "column" }}>
-				<Button
-					variant="contained"
-					sx={{ mb: 2, paddingX: 3 }}
-					color="primary"
-					startIcon={<ContactPageOutlined />}>
-					{/* <FavoriteOutlined /> */}
-					Biografia
-				</Button>
-				<Button
-					variant="contained"
-					sx={{ mb: 2, paddingX: 3 }}
-					color="error"
-					startIcon={<FavoriteBorderOutlined />}>
-					{/* <FavoriteOutlined /> */}
-					Seguir
-				</Button>
-			</Box>
-		</Paper>
-	);
+        <Paper
+            elevation={4}
+            sx={{
+                paddingY: 3,
+                paddingX: 4,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+            }}>
+            <Box>
+                <Avatar
+                    alt={artista.interprete}
+                    src={`/img/artistas/${artista.imgPortada}`}
+                    sx={{
+                        width: 150,
+                        height: 150,
+                        border: "2px solid #DE260E",
+                    }}
+                />
+            </Box>
+            <Box>
+                <Typography variant="h1" component="h1" color="initial">
+                    {artista.interprete}
+                </Typography>
+                <List sx={{ textAlign: "center" }}>
+                    {/* <ListItem> */}
+                    {tipos}
+                    {/* </ListItem> */}
+                </List>
+                <Box
+                    sx={{
+                        mt: 1,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "GrayText",
+                    }}>
+                    <FavoriteBorderOutlined
+                        fontSize="small"
+                        sx={{ pr: "3px", color: "GrayText" }}
+                    />
+                    {artista.seguidores} Seguidores
+                </Box>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Button
+                    variant="contained"
+                    sx={{ mb: 2, paddingX: 3 }}
+                    color="primary"
+                    startIcon={<ContactPageOutlined />}>
+                    {/* <FavoriteOutlined /> */}
+                    Biografia
+                </Button>
+                <Button
+                    variant="contained"
+                    sx={{ mb: 2, paddingX: 3 }}
+                    color="error"
+                    startIcon={<FavoriteBorderOutlined />}>
+                    {/* <FavoriteOutlined /> */}
+                    Seguir
+                </Button>
+            </Box>
+        </Paper>
+    );
 }
---
+++ components/audios/AudioList.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { Grid } from '@mui/material';
+
+// interface Props {
+//     // audios: IAudio[];
+// }
+
+// export const AudioList: FC<Props> = ({audios}) => {
+//     return (
+//         <Grid container spacing={4}>
+//             {/* {audios.map((audio) => (
+//                 // poner el id
+//                 <AudioCard key={audio.slug} audio={audio} />
+//             ))} */}
+//         </Grid>
+//     );
+// };
